df313ff50d2d7ab0ae3cd051578e9a76ae8cc273
tonyykosseifyy <tonykosseify123@gmail.com>
2021-03-06 15:27:56 +0200

    working on the upper body component

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,12 +1,22 @@
 import React from 'react'; 
 import './Body.css' ;
-
+import { heroDesktop } from '../Assets/logo' ;
+import Button from './Button' ;
 
 const Body = ({ open }) => {
   return (
     <main className='body'>
       <div className={`overlay ${open && 'start-overlay'}`}></div>
-      <div className='body-up'> </div>
+      <div className='body-up'> 
+        <div className='body-up-left'>
+          <h1>Next generation digital banking</h1>
+          <p>Take your financial life online. Your Easy Bank account will be a one-stop-shop for spending , saving , budgeting , investing and much more</p>
+          <Button />
+        </div>
+        <div className='body-up-right'>
+          { heroDesktop }
+        </div>
+      </div>
     </main> 
   )
 } ;
