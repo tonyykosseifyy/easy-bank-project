d4429c498e976c8f68cc14f06369f2f3f794ab00
Tony Kosseify <tonykosseify123@gmail.com>
2021-03-08 16:03:53 +0200

    upper body component done

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,22 +1,23 @@
 import React from 'react'; 
 import './Body.css' ;
-import { heroDesktop } from '../Assets/logo' ;
 import Button from './Button' ;
+import imageMockup from '../Assets/image-mockups.png'
+
 
 const Body = ({ open }) => {
   return (
     <main className='body'>
-      <div className={`overlay ${open && 'start-overlay'}`}></div>
-      <div className='body-up'> 
-        <div className='body-up-left'>
-          <h1>Next generation digital banking</h1>
-          <p>Take your financial life online. Your Easy Bank account will be a one-stop-shop for spending , saving , budgeting , investing and much more</p>
-          <Button />
-        </div>
-        <div className='body-up-right'>
-          { heroDesktop }
-        </div>
-      </div>
+    	<div className={`overlay ${open && 'start-overlay'}`}></div>
+      	<div className='body-up'> 
+        	<div className='body-up-left'>
+				<h1>Next generation digital banking</h1>
+				<p>Take your financial life online. Your Easy Bank account will be a one-stop-shop for spending , saving , budgeting , investing and much more</p>
+				<Button />
+        	</div>
+			<div className='body-up-right'>
+				<img src={imageMockup} alt='decoration' />
+			</div>
+    	</div>
     </main> 
   )
 } ;
